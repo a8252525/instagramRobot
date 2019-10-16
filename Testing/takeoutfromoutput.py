436f2d8b52818dd9dd9c0a039b9677d24cf17255
AntonyKo <a8252525@gmail.com>
2019-10-16 16:12:07 +0800

    Keep testing

--- Testing/takeoutfromoutput.py
+++ Testing/takeoutfromoutput.py
@@ -5,16 +5,18 @@ Created on Tue Sep 17 00:34:35 2019
 @author: ideapad 320
 """
 import re
-import request
+import requests
 
 n=100
 with open ('C:/Users/ideapad 320/Desktop/IG機器人/instragram-crawler/instagram-crawler/output.txt','r',encoding="utf-8") as data:
     for i in range(n):
-        pattern=re.complie(r'')
-        img_url=re.findall('key',{'img_url'})
-        
-        for link in img_alt:
-            url=link['src']
+        pattern=re.compile(r'"img_url":.*"}')
+        data_read=data.read()
+        print(data_read)
+        img_url=pattern.find(data_read)
+        print(img_url)
+        for url in img_url:
+
             with requests.get(url, stream=True) as r:
                 image_name=url.split('/')[-1]
                 with open('C:/Users/ideapad 320/Desktop/IG機器人/data/{}'.format(image_name),'wb') as f:
